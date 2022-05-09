0d572f16be4d5c54b78d4c24b077026448d6a807
Ethan McTague <ethan@tague.me>
2022-05-09 17:46:11 -0400

    Allow manipulation in scene inspector

--- RuntimeInjectedCode/SceneViewCheatScreen.cs
+++ RuntimeInjectedCode/SceneViewCheatScreen.cs
@@ -26,7 +26,19 @@ namespace RuntimeInjectedCode
             {
                 if (GUILayout.Button("<--- Back (Inspecting: " + _inspecting.name + ")"))
                 {
-                    _inspecting = _inspecting.transform.parent;
+                    _inspecting = _inspecting.parent;
+                }
+
+                if (GUILayout.Button("! DELETE THIS OBJECT !"))
+                {
+                    var prevInsp = _inspecting;
+                    _inspecting = _inspecting.parent;
+                    Destroy(prevInsp.gameObject);
+                }
+
+                if (GUILayout.Button("Enabled: " + _inspecting.gameObject.activeSelf))
+                {
+                    _inspecting.gameObject.SetActive(!_inspecting.gameObject.activeSelf);
                 }
 
                 foreach (var c in _inspecting.GetComponents(typeof(Component)))
@@ -34,7 +46,7 @@ namespace RuntimeInjectedCode
                     GUILayout.Label("Component: " + c.GetType().Name);
                 }
 
-                foreach (Transform child in _inspecting.transform)
+                foreach (Transform child in _inspecting)
                 {
                     if (GUILayout.Button("Child: " + child.name))
                     {
